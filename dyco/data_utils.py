ea031744f8b1d6dc59d9e530f59af7d042afff00
Dongkwan Kim <todoaskit@gmail.com>
2021-07-27 22:31:49 +0900

    Improve comments in data_utils

--- dyco/data_utils.py
+++ dyco/data_utils.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import torch
 from torch import Tensor
-from torch_geometric.data import Batch, Data
+from torch_geometric.data import Batch, Data, TemporalData
 
 
 def to_singleton_data(data_list) -> Data:
@@ -8,10 +10,14 @@ def to_singleton_data(data_list) -> Data:
     return Data(**{k: getattr(b, k) for k in b.keys})
 
 
-def from_events_to_singleton_data(data_list):
-    # (subject entity, relation, object, entity, time)
-    #   --> (edge_index=[sub, obj], ...)
-    # e.g., Data(obj=[373018], rel=[373018], sub=[373018], t=[373018])
+def from_events_to_singleton_data(data_list: List[Data]) -> Data:
+    """Convert (subject entity, relation, object, entity, time)
+        e.g., Data(obj=[373018], rel=[373018], sub=[373018], t=[373018]
+        --> (edge_index=[sub, obj], ...)
+    :param data_list: List[Data]
+    :return: Data,
+        e.g., Data(edge_index=[2, 373018], rel=[373018, 1], t=[373018, 1])
+    """
     s_data = to_singleton_data(data_list)
     edge_index = torch.stack([s_data.sub, s_data.obj])
     rel, t = s_data.rel.view(-1, 1), s_data.t.view(-1, 1)
