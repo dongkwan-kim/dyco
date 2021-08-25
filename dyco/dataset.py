b253eb4d937b98141013a98be8bdcc558f0a559b
Dongkwan Kim <todoaskit@gmail.com>
2021-08-25 19:11:33 +0900

    Minor updates in dataset.py

--- dyco/dataset.py
+++ dyco/dataset.py
@@ -21,6 +21,7 @@ from data_utils import from_events_to_singleton_data, ToTemporalData
 class SingletonICEWS18(ICEWS18):
 
     def __init__(self, root, split='train', transform=None, pre_transform=None, pre_filter=None):
+        self.split = split
         super(SingletonICEWS18, self).__init__(root, split, transform, pre_transform, pre_filter)
 
     def download(self):
@@ -33,11 +34,15 @@ class SingletonICEWS18(ICEWS18):
         torch.save(self.collate([from_events_to_singleton_data(data_list[s[1]:s[2]])]), self.processed_paths[1])
         torch.save(self.collate([from_events_to_singleton_data(data_list[s[2]:s[3]])]), self.processed_paths[2])
 
+    def __repr__(self):
+        return "{}(split={})".format(self.__class__.__name__, self.split)
+
 
 class SingletonGDELT(GDELT):
 
     def __init__(self, root, split='train', transform=None, pre_transform=None, pre_filter=None):
         self.url = 'https://github.com/INK-USC/RE-Net/raw/master/data/GDELT'
+        self.split = split
         super(SingletonGDELT, self).__init__(root, split, transform, pre_transform, pre_filter)
 
     def download(self):
@@ -49,6 +54,9 @@ class SingletonGDELT(GDELT):
         torch.save(self.collate([from_events_to_singleton_data(data_list[s[0]:s[1]])]), self.processed_paths[0])
         torch.save(self.collate([from_events_to_singleton_data(data_list[s[1]:s[2]])]), self.processed_paths[1])
         torch.save(self.collate([from_events_to_singleton_data(data_list[s[2]:s[3]])]), self.processed_paths[2])
+
+    def __repr__(self):
+        return "{}(split={})".format(self.__class__.__name__, self.split)
 
 
 def _get_dataset_at_cls_dir(cls, path, transform=None, pre_transform=None, *args, **kwargs):
@@ -66,10 +74,19 @@ def get_dynamic_graph_dataset(path, name: str, transform=None, pre_transform=Non
                                                 name=sub_name, *args, **kwargs)
         jodie_dataset.num_nodes = jodie_dataset[0].dst.max().item() + 1
         return jodie_dataset
-    elif name in ["BitcoinOTC", "SingletonICEWS18", "SingletonGDELT"]:
-        return _get_dataset_at_cls_dir(eval(name), path, transform=transform, pre_transform=pre_transform,
-                                       *args, **kwargs)
-    elif name.startswith("ogbn") or name.startswith("ogbl"):
+    elif name in ["SingletonICEWS18", "SingletonGDELT"]:
+        args_wo_split = (eval(name), path, *args)
+        kwargs_wo_split = dict(transform=transform, pre_transform=pre_transform, **kwargs)
+        train_set = _get_dataset_at_cls_dir(*args_wo_split, **kwargs_wo_split, split="train")
+        val_set = _get_dataset_at_cls_dir(*args_wo_split, **kwargs_wo_split, split="val")
+        test_set = _get_dataset_at_cls_dir(*args_wo_split, **kwargs_wo_split, split="test")
+        return train_set, val_set, test_set
+    elif name in ["BitcoinOTC"]:
+        # todo: train-val-test split
+        # return _get_dataset_at_cls_dir(eval(name), path, transform=transform, pre_transform=pre_transform,
+        #                                *args, **kwargs)
+        raise NotImplementedError
+    elif name.startswith("ogb"):
         cls = PygNodePropPredDataset if name.startswith("ogbn") else PygLinkPropPredDataset
         return _get_dataset_at_cls_dir(cls, path, transform=transform, pre_transform=pre_transform,
                                        name=name, *args, **kwargs)
@@ -79,7 +96,7 @@ def get_dynamic_graph_dataset(path, name: str, transform=None, pre_transform=Non
 
 if __name__ == '__main__':
     PATH = "/mnt/nas2/GNN-DATA/PYG/"
-    NAME = "JODIEDataset/wikipedia"
+    NAME = "SingletonICEWS18"
     # JODIEDataset/reddit, JODIEDataset/wikipedia, JODIEDataset/mooc, JODIEDataset/lastfm
     #   TemporalData(dst=[157474], msg=[157474, 172], src=[157474], t=[157474], y=[157474])
     # ogbn-arxiv, ogbl-collab, ogbl-citation2
@@ -95,6 +112,8 @@ if __name__ == '__main__':
         PATH, NAME,
         # transform=ToTemporalData(),  # if necessary.
     )
+    if isinstance(_dataset, tuple):
+        _dataset = _dataset[0]
     print(_dataset)
     for d in _dataset:
         print(d)
