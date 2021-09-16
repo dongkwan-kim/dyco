bd6607fecd61696f8d7c79528de0986f525eb274
Dongkwan Kim <todoaskit@gmail.com>
2021-09-16 17:40:40 +0900

    Update pyg

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ pip3 install torch==${TORCH}
 pip3 install torch-scatter==2.0.8 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip3 install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip3 install torch-cluster==1.5.9 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
-pip3 install torch-geometric==2.0.0
+pip3 install torch-geometric==2.0.1
 pip3 install -r requirements.txt
 echo "Install completed"
