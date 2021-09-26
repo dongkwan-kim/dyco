579f4cf5284f8ead17c602230305d97a50248922
Dongkwan Kim <todoaskit@gmail.com>
2021-09-27 01:54:16 +0900

    Refactoring

--- dyco/run_main.py
+++ dyco/run_main.py
@@ -1,18 +1,116 @@
+from typing import List, Optional
+
 import hydra
 from omegaconf import DictConfig
+from pytorch_lightning import (
+    Callback,
+    LightningDataModule,
+    LightningModule,
+    Trainer,
+    seed_everything,
+)
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from run_utils import get_logger, log_hyperparameters, finish
 
 
 """Codes are adopted from
+    https://github.com/ashleve/lightning-hydra-template/blob/main/src/train.py
     https://github.com/ashleve/lightning-hydra-template/blob/main/run.py"""
 
 
-@hydra.main(config_path="configs/", config_name="config.yaml")
+log = get_logger(__name__)
+
+
+def train(config: DictConfig) -> Optional[float]:
+    """Contains training pipeline.
+    Instantiates all PyTorch Lightning objects from config.
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+    Returns:
+        Optional[float]: Metric score for hyperparameter optimization.
+    """
+
+    # Set seed for random number generators in pytorch, numpy and python.random
+    if "seed" in config:
+        seed_everything(config.seed, workers=True)
+
+    # Init lightning datamodule
+    log.info(f"Instantiating datamodule <{config.datamodule._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(config.datamodule)
+
+    # Init lightning model
+    log.info(f"Instantiating model <{config.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(config.model)
+
+    # Init lightning callbacks
+    callbacks: List[Callback] = []
+    if "callbacks" in config:
+        for _, cb_conf in config.callbacks.items():
+            if "_target_" in cb_conf:
+                log.info(f"Instantiating callback <{cb_conf._target_}>")
+                callbacks.append(hydra.utils.instantiate(cb_conf))
+
+    # Init lightning loggers
+    logger: List[LightningLoggerBase] = []
+    if "logger" in config:
+        for _, lg_conf in config.logger.items():
+            if "_target_" in lg_conf:
+                log.info(f"Instantiating logger <{lg_conf._target_}>")
+                logger.append(hydra.utils.instantiate(lg_conf))
+
+    # Init lightning trainer
+    log.info(f"Instantiating trainer <{config.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(
+        config.trainer, callbacks=callbacks, logger=logger, _convert_="partial"
+    )
+
+    # Send some parameters from config to all lightning loggers
+    log.info("Logging hyperparameters!")
+    log_hyperparameters(
+        config=config,
+        model=model,
+        datamodule=datamodule,
+        trainer=trainer,
+        callbacks=callbacks,
+        logger=logger,
+    )
+
+    # Train the model
+    log.info("Starting training!")
+    trainer.fit(model=model, datamodule=datamodule)
+
+    # Evaluate model on test set, using the best model achieved during training
+    if config.get("test_after_training") and not config.trainer.get("fast_dev_run"):
+        log.info("Starting testing!")
+        trainer.test()
+
+    # Make sure everything closed properly
+    log.info("Finalizing!")
+    finish(
+        config=config,
+        model=model,
+        datamodule=datamodule,
+        trainer=trainer,
+        callbacks=callbacks,
+        logger=logger,
+    )
+
+    # Print path to best checkpoint
+    log.info(f"Best checkpoint path:\n{trainer.checkpoint_callback.best_model_path}")
+
+    # Return metric score for hyperparameter optimization
+    optimized_metric = config.get("optimized_metric")
+    if optimized_metric:
+        return trainer.callback_metrics[optimized_metric]
+
+
+@hydra.main(config_path="../configs/", config_name="config.yaml")
 def main(config: DictConfig):
 
     # Imports should be nested inside @hydra.main to optimize tab completion
     # Read more here: https://github.com/facebookresearch/hydra/issues/934
     import run_utils
-    from run_train import train
 
     # A couple of optional utilities:
     # - disabling python warnings
